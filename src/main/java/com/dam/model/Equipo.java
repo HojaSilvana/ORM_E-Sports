fbf1839a006d44dbfaf11183eda4768e724347b3
HojaSilvana <david.cuenca.bascunana@gmail.com>
2026-01-19 14:13:05 +0100

    Conversión de la clase Equipo en entidad JPA

--- src/main/java/com/dam/model/Equipo.java
+++ src/main/java/com/dam/model/Equipo.java
@@ -1,4 +1,58 @@
 package com.dam.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+/**
+ * Esta clase se encarga de representar un equipo de la competición.
+ *
+ * @author David Cuenca
+ */
+
+@Entity
 public class Equipo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String region;
+
+    public Equipo() {
+    }
+
+    public Equipo(Long id, String nombre, String region) {
+        this.id = id;
+        this.nombre = nombre;
+        this.region = region;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
 }
+
+
+
+
